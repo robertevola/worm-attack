c502ce02e7e244f4555cc3744981ad8314568211
fueltherage <wesleyallard1990@gmail.com>
2015-01-24 16:40:29 -0500

    no message

--- Assets/Scripts/Worm/WormPiece.cs
+++ Assets/Scripts/Worm/WormPiece.cs
@@ -10,7 +10,7 @@ public class WormPiece : WormEntity
 	public float Speed = 1.0f;
   	
 
-	float holeEntryScale = 2f;
+	float holeEntryScale = 0.5f;
 
 	// Use this for initialization
 	void Start () 
@@ -41,22 +41,22 @@ public class WormPiece : WormEntity
 				//transform.Translate(dif.normalized * (dif.magnitude - radius), Space.World);
 			}
 		}
-		//		if(base.isUnderground)
-		//		{
-		//			//Debug.Log ("BOOYEAH");
-		//			Vector3 holeScale = new Vector3();
-		//			holeScale.x = Mathf.Lerp(transform.localScale.x, holeEntryScale, 0.75f);
-		//			holeScale.y = Mathf.Lerp(transform.localScale.y, holeEntryScale, 0.75f);
-		//			holeScale.z =  Mathf.Lerp(transform.localScale.z, holeEntryScale, 0.75f);
-		//			transform.localScale = holeScale;
-		//		}else
-		//		{
-		//			Vector3 holeScale = new Vector3();
-		//			holeScale.x = Mathf.Lerp(transform.localScale.x, 3.0f, 0.75f);
-		//			holeScale.y = Mathf.Lerp(transform.localScale.y, 3.0f, 0.75f);
-		//			holeScale.z = Mathf.Lerp(transform.localScale.z, 3.0f, 0.75f);
-		//			transform.localScale = holeScale;
-		//		}
+		if(base.isUnderground)
+		{
+			
+			Vector3 holeScale = new Vector3();
+			holeScale.x = Mathf.Lerp(transform.localScale.x, holeEntryScale, 0.75f);
+			holeScale.y = Mathf.Lerp(transform.localScale.y, holeEntryScale, 0.75f);
+			holeScale.z =  Mathf.Lerp(transform.localScale.z, holeEntryScale, 0.75f);
+			transform.localScale = holeScale;
+		}else
+		{
+			Vector3 holeScale = new Vector3();
+			holeScale.x = Mathf.Lerp(transform.localScale.x, 1.0f, 0.75f);
+			holeScale.y = Mathf.Lerp(transform.localScale.y, 1.0f, 0.75f);
+			holeScale.z = Mathf.Lerp(transform.localScale.z, 1.0f, 0.75f);
+			transform.localScale = holeScale;
+		}
 	}
 
     public void SetSprite(Sprite sprite)
