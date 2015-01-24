dbbcf1f6d5c2b48d2de3f573935bc41aa82305a7
Robert Evola <robert.evola@gmail.com>
2015-01-23 23:36:09 -0500

    Input scripts, Bases for Input GUI, Outline for worm scripts

---
+++ Assets/Scripts/Worm/WormPiece.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class WormPiece : MonoBehaviour 
+{
+    private SpriteRenderer spriteRenderer;
+    public WormPiece next, previous;
+
+    public bool isUnderground;
+
+	// Use this for initialization
+	void Start () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    public void SetSprite(Sprite sprite)
+    {
+        spriteRenderer.sprite = sprite;
+    }
+}
