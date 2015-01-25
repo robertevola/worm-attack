d9bbb607a3af328d6e0a18aaf617e430f6a8c949
fueltherage <wesleyallard1990@gmail.com>
2015-01-25 08:57:52 -0500

    kill script and human death animation

---
+++ Assets/KillScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour {
+
+	public float timeToDie = 1.0f;
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, timeToDie);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
